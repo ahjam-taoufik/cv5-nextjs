65cb45fa9b54d3e7d440fe107ccfab29c959532c
Ahjam Taoufik <taoufikahjam@gmail.com>
2022-11-02 00:41:27 +0100

    add event detail

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,27 @@
+import { useRouter } from 'next/router'
 import React from 'react'
+import {getEventById} from '../../dummy-data'
+import EventSummary from '../../components/event-detail/event-summary'
+import EventContent from '../../components/event-detail/event-content'
+import EventLogistic from '../../components/event-detail/event-logistics'
+
 
 function EventDetailPage() {
+  const route=useRouter()
+  const id=route.query.eventId
+
+  const event=getEventById(id)
+ 
+
+  if (!event) {
+    return <p>  Event not found</p>
+  }
+
   return (
-    <div>EventDetailPage</div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistic date={event.date}  address={event.location} image={event.image} imageAlt={event.title} />
+    </>
   )
 }
 
